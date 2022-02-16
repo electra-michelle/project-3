c3b732f6238593d32c30b3af4798ad7c647ffdc5
vagrant <vagrant@homestead>
2022-02-16 10:48:52 +0000

    WIP

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,6 @@
 
 namespace App\Http\Controllers;
 
-
-use \IEXBase\TronAPI\Tron;
-use \IEXBase\TronAPI\Provider\HttpProvider;
-
 class HomeController extends Controller
 {
 
@@ -21,19 +17,7 @@ class HomeController extends Controller
 
     public function test()
     {
-        $fullNode = new HttpProvider('https://api.trongrid.io');
-        $solidityNode = new HttpProvider('https://api.trongrid.io');
-        $eventServer = new HttpProvider('https://api.trongrid.io');
-        try {
-            $tron = new Tron($fullNode, $solidityNode, $eventServer);
-        } catch (\IEXBase\TronAPI\Exception\TronException $e) {
-            exit($e->getMessage());
-        }
 
-        $acc = $tron->createAccount();
-        dd($acc);
-//        $contract = $tron->contract('TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t');
 
-//        dd($contract->balanceOf());
     }
 }
