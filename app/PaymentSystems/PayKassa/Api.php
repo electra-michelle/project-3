e290644346420af4c353791f84e6a4b93c6d0530
root <root@nc-ph-2083-45.web-hosting.com>
2022-02-17 10:07:48 -0800

    WIP

--- app/PaymentSystems/PayKassa/Api.php
+++ app/PaymentSystems/PayKassa/Api.php
@@ -63,5 +63,55 @@ class Api extends BasePayKassa  implements PayKassaInterface
             return json_decode(json_encode(['error' => $exception->getMessage()]));
         }
     }
+	
+	
+	public function send($wallet, $amount, $currency, $paymentSystemValue, $data = [])
+	{
+		
+		$systemId = [
+			"perfectmoney" => 2, // supported currencies USD    
+			"berty" => 7, // supported currencies RUB, USD    
+			"bitcoin" => 11, // supported currencies BTC    
+			"ethereum" => 12, // supported currencies ETH    
+			"litecoin" => 14, // supported currencies LTC    
+			"dogecoin" => 15, // supported currencies DOGE    
+			"dash" => 16, // supported currencies DASH    
+			"bitcoincash" => 18, // supported currencies BCH    
+			"zcash" => 19, // supported currencies ZEC    
+			"ripple" => 22, // supported currencies XRP    
+			"tron" => 27, // supported currencies TRX    
+			"stellar" => 28, // supported currencies XLM    
+			"binancecoin" => 29, // supported currencies BNB    
+			"tron_trc20_usdt" => 30, // supported currencies USDT    
+			"binancesmartchain_bep20" => 31, // supported currencies USDT, BUSD, USDC, ADA, EOS, BTC, ETH, DOGE    
+			"ethereum_erc20" => 32, // supported currencies USDT    
+		];
+		
+		try {
+			$balance = $this->makeRequest('', [
+				'func' => 'api_payment',
+				'shop' => config('paykassa.sci.id'),
+				'amount'   => $amount,
+				'currency' => $currency,
+				'system' => $systemId[$paymentSystemValue],
+				'paid_commission' => $data['commission'] ?? '',
+				'number' => $wallet,
+				'tag' => $data['tag'] ?? '',
+				'priority' =>  $data['priority'] ?? '',
+			]);
+			
+			if($balance->error) {
+				return [
+					'error' => true,
+					'message' => $balance->message
+				];
+			}		
+			
+			return ['error' => false];
+		} catch (\Exception $exception) {
+			return json_decode(json_encode(['error' => $exception->getMessage()]));
+		}
+	}
+
 
 }
